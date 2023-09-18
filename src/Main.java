de6cbad696b04038587ae8d8a6716f519a418330
fitz3 <fitz3kat@gmail.com>
2023-09-17 20:13:07 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //int purchasePrice, double salesTax
+        int purchasePrice = 27;
+        double salesTax = purchasePrice * .05;
+        System.out.println("The sales tax on a " + purchasePrice + " item would be " + salesTax);
+
+    }
+}
